afcb1cdacd021e490bea87eb3eeb82acd17cd90d
Abel Zewdie <Abelmzewdie@gmail.com>
2024-04-10 13:46:09 -0400

    WIP

---
+++ Script/main.js
@@ -0,0 +1,8 @@
+//OBJECT.METHOD parameters( EVENT_NAME, HANDLER_FUNCTION)
+document.addEventListener("DOMContentLoaded", () => {
+
+  //DOM - document object model
+  extra_special.innerHTML = "Replacement Text"
+  extra_special.style.color = "red";
+
+}); //END DOM CONTENT LOADED
